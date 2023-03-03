d24c630024ad38282fa2b47fcb7e13f5568f0ebf
Julio Branda <kolstomier@protonmail.com>
2023-03-02 21:12:36 -0300

    ejercico tema 1 java basico

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //1 NUMEROS
+
+        //1.1 numeros enteros
+        byte variableByte = 9;
+        System.out.println(variableByte);
+
+        short variableShort = 13;
+        System.out.println(variableShort);
+
+        int variableInt = 27;
+        System.out.println(variableInt);
+
+        long variableLong = 4321;
+        System.out.println(variableLong);
+
+        //1.2 numeros decimales
+
+        float variableFloat = 7.9f;
+        System.out.println(variableFloat);
+
+        double variableDouble = 342.7d;
+        System.out.println(variableDouble);
+
+        //2 BOOLEANOS
+
+        boolean variableBoolT = true;
+        System.out.println(variableBoolT);
+
+        boolean variableBoolF = false;
+        System.out.println(variableBoolF);
+
+        //3 TEXTO
+
+        char variableChar = 'q';
+        System.out.println(variableChar);
+
+        String variableStr = "Parole, parole, parole... ";
+        System.out.println(variableStr);
+
+    }
+}
